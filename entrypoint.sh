8597a4f505d418e9eb1830558404fb0c2a19f8ba
Parker Wahle <regulad@regulad.xyz>
2024-06-30 13:54:49 -0400

    verbose entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@
 # a lot of the code in here will be borrowed from Stringray, the last project I did with remote desktops and servers
 # https://github.com/regulad/stingray/blob/master/stingray.py
 
+echo "Rotating TLS key and certificate for wayvnc..."
 # just rotate the key every time, it's not like it's a big deal
 cd ~/.config/wayvnc || exit 1
 rm -f tls_key.pem tls_cert.pem
@@ -19,28 +20,23 @@ openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:secp384r1 -sha384 \
 cd ~ || exit 1
 
 # envsubst
+echo "Injecting environment variables into wayvnc config..."
 rm -f ~/.config/wayvnc/config || exit 1
 envsubst < ~/.config/wayvnc/config_template > ~/.config/wayvnc/config || exit 1
 
-function wait_for_device() {
-    while true; do
-        adb devices | grep -q "$DEVICE_IP:$DEVICE_ADB_PORT" && break
-        sleep 1
-    done
-}
-
+echo "Connecting to ADB device..."
 adb connect "$DEVICE_IP":"$DEVICE_ADB_PORT" || exit 1
-wait_for_device
 
 # running
 
 function open_scrcpy() {
-  export SDL_VIDEODRIVER=wayland  # satisfies scrcpy and tells it to use wayland
-  cage scrcpy || exit 1
+  echo "Starting scrcpy..."
+  SDL_VIDEODRIVER=wayland cage scrcpy || exit 1
 }
 
 function open_wayvnc() {
   sleep 10 # give cage time to start scrcpy
+  echo "Starting wayvnc... (container will be ready soon)"
   wayvnc 0.0.0.0 || exit 1
 }
 
